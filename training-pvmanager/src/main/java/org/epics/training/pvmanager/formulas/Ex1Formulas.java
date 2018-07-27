61202c4f5b313625689814a465dad9c78c6f459b
Kunal Shroff <kunalshroff9@gmail.com>
2018-07-27 11:43:10 -0400

    Add a section for formula functions

---
+++ training-pvmanager/src/main/java/org/epics/training/pvmanager/formulas/Ex1Formulas.java
@@ -0,0 +1,21 @@
+package org.epics.training.pvmanager.formulas;
+
+/**
+ * Creating formula function to process the data coming from the datasource
+ * before notifying the client.
+ * 
+ * @author Kunal Shroff
+ *
+ */
+public class Ex1Formulas {
+
+    /**
+     * Create a formula function for extracting an element at a given index from a
+     * waveform
+     */
+
+    /**
+     * Create an formula function that maps a pvStructure to a java map
+     * 
+     */
+}

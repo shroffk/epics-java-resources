1d56650af92d6dfdc577cfd54929b53ff3133942
Kunal Shroff <kunalshroff9@gmail.com>
2018-07-27 11:20:15 -0400

    Advance training set for gpclient

---
+++ training-gpclient/src/main/java/org/epics/training/gpclient/infrastructure/Ex2CreatingReadCollector.java
@@ -0,0 +1,12 @@
+package org.epics.training.gpclient.infrastructure;
+
+/**
+ * Explore the possibility of putting together a Read collector populated from
+ * any data source and made available to gpClient channels
+ * 
+ * @author Kunal Shroff
+ *
+ */
+public class Ex2CreatingReadCollector {
+
+}
---
+++ training-gpclient/src/main/java/org/epics/training/gpclient/infrastructure/Introduction.java
@@ -0,0 +1,10 @@
+package org.epics.training.gpclient.infrastructure;
+
+/**
+ * An introduction to the core 
+ * @author Kunal Shroff
+ *
+ */
+public class Introduction {
+
+}

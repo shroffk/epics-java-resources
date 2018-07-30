34cddc08a782f235cd48ab40829ea55bc8976190
Kunal Shroff <kunalshroff9@gmail.com>
2018-07-30 10:35:08 -0400

    Adding details to the gp client read ex

--- training-gpclient/src/main/java/org/epics/training/gpclient/Ex2Read.java
+++ training-gpclient/src/main/java/org/epics/training/gpclient/Ex2Read.java
@@ -5,12 +5,20 @@ public class Ex2Read {
     public static void main(String[] args) {
 
         // Create a channel to monitor a channel which produces scalar values
+        // 1. request a known type Double
+        // 2. request a known type String
+        // 3. request a known type Enum
+        // 4. request a channel of type Number. The Number can be used to handle all
+        // numerical types
 
         // Create a channel to read a single scalar value once from a channel
 
         // Create a channel to monitor a channel which produces scalar array values
 
         // Create a channel to read a single scalar array value once from a channel
+
+        // Error conditions:
+        // 1. mismatch type
     }
 
 }

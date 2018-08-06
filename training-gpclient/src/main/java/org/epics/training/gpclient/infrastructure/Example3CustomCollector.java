ee0d054e90185f7665500a777cb9aa0575089641
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2018-08-06 14:48:57 -0400

    Finish the example

--- training-gpclient/src/main/java/org/epics/training/gpclient/infrastructure/Example3CustomCollector.java
+++ training-gpclient/src/main/java/org/epics/training/gpclient/infrastructure/Example3CustomCollector.java
@@ -1,5 +1,6 @@
 package org.epics.training.gpclient.infrastructure;
 
+import java.time.Duration;
 import java.util.function.Consumer;
 import org.epics.gpclient.CollectorExpression;
 import org.epics.gpclient.GPClient;
@@ -8,6 +9,8 @@ import org.epics.gpclient.PVReader;
 import org.epics.gpclient.ReadCollector;
 import org.epics.vtype.VNumber;
 import org.epics.vtype.VType;
+
+import static org.epics.gpclient.GPClient.*;
 
 /**
  * 
@@ -27,7 +30,10 @@ public class Example3CustomCollector {
         @Override
         public VNumber getValue() {
             synchronized(lock) {
-                return value;
+                // Returns the value and clears the queue
+                VNumber latestValue = value;
+                value = null;
+                return latestValue;
             }
         }
 
@@ -35,6 +41,7 @@ public class Example3CustomCollector {
         public void updateValue(VNumber newValue) {
             Consumer<PVEvent> listener;
             synchronized(lock) {
+                // Compare the new value with the old and keep the highest
                 if (value == null) {
                     value = newValue;
                 } else if (newValue == null) {
@@ -58,6 +65,26 @@ public class Example3CustomCollector {
         
     }
     
+    public static ReadCollector<VNumber, VNumber> maxWithinBurst() {
+        return new MaxCollector();
+    }
+    
     public static void main(String[] args) throws InterruptedException {
+        // Reads a channel that picks a random number between 0 and 1 100 times
+        // a second. Notify updates at 2 Hz and return the maximum with the 
+        // update interval.
+        PVReader<VNumber> noise = GPClient.read(channel("sim://noise(0,1,0.01)", maxWithinBurst()))
+                .addReadListener((event, pvReader) -> {
+                    System.out.println(event + " " + pvReader.getValue());
+                })
+                .maxRate(Duration.ofMillis(500))
+                .start();
+
+        // Compare when using cacheLastValue(VNumber.class) instead of maxWithinBurst()
+        // and see the value oscillates around the average
+        
+        Thread.sleep(5000);
+        
+        noise.close();
     }
 }
